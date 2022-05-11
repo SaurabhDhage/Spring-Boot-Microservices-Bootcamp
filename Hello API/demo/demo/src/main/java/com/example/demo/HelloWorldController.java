7500d4b49f2f486f3e1a6c6444e14b4fd9e2e31a
Saurabh Dhage <saurabh.dhage@mindstix.com>
2022-05-11 12:19:36 +0530

    added HelloAPI

---
+++ Hello API/demo/demo/src/main/java/com/example/demo/HelloWorldController.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+// http://localhost:8080/hello-world
+@RestController
+public class HelloWorldController {
+    @GetMapping("/hello-world")
+    public String HelloWorld(){
+        return "Hello World";
+    }
+}
