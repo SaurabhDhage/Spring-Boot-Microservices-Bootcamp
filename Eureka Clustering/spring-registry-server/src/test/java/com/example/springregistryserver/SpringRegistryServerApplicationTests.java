144d780b402f4c1514c660842bbb1662b9b67b2f
Saurabh Dhage <saurabhdhage999@gmail.com>
2022-05-13 22:04:22 +0530

    added Eureka Clustering

---
+++ Eureka Clustering/spring-registry-server/src/test/java/com/example/springregistryserver/SpringRegistryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springregistryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRegistryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
